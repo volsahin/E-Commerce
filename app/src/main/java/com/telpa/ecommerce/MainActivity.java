c22de000a586d3eb3a333998356cfdc05c1753e6
volkan <volkansahin95@gmail.com>
2016-07-12 14:14:42 +0300

    Right Top Menu

--- app/src/main/java/com/telpa/ecommerce/MainActivity.java
+++ app/src/main/java/com/telpa/ecommerce/MainActivity.java
@@ -2,11 +2,18 @@ package com.telpa.ecommerce;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.right_top,menu);
+        return true;
     }
 }
