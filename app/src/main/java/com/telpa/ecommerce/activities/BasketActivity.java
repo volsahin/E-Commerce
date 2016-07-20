bb8f0abcdc2ed18eb85df08b3c704a6dd7f30a9f
musacibik <mcibik13@ku.edu.tr>
2016-07-20 09:41:24 +0300

    Basket activity üzerinde çalışılıyor.

--- app/src/main/java/com/telpa/ecommerce/activities/BasketActivity.java
+++ app/src/main/java/com/telpa/ecommerce/activities/BasketActivity.java
@@ -1,39 +1,36 @@
 package com.telpa.ecommerce.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
-import android.widget.ListView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 
 import com.telpa.ecommerce.R;
-import com.telpa.ecommerce.adapters.BasketListAdapter;
-import com.telpa.ecommerce.models.BasketList;
-import com.telpa.ecommerce.models.HList;
-import com.telpa.ecommerce.models.IList;
+import com.telpa.ecommerce.adapters.RecyclerAdapter;
 import com.telpa.ecommerce.utils.BaseActivity;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class BasketActivity extends BaseActivity {
-    public List<BasketList> list = new ArrayList<BasketList>();
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter recyclerAdapter;
+    private RecyclerView.LayoutManager recyclerLayoutManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basket);
 
-        fcreateTitle("My Store");
+        fcreateTitle("Your Basket");
         fcreateToolbar(this, true, false, R.id.toolbar);
         fcreateMenu(this, false);
 
+        recyclerView = (RecyclerView) findViewById(R.id.basketList);
 
-        list.add(new BasketList("Product1", "$100"));
-        list.add(new BasketList("Product2", "$100"));
-        list.add(new BasketList("Product3", "$100"));
-        list.add(new BasketList("Product4", "$100"));
+        recyclerView.setHasFixedSize(true);
+        recyclerLayoutManager = new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(recyclerLayoutManager);
 
-        BasketListAdapter adapter=new BasketListAdapter(this, list);
-        ListView listView=(ListView) findViewById(R.id.basketList);
-        listView.setAdapter(adapter);
+        recyclerView.addItemDecoration(new RecyclerAdapter.SpaceItemDecoration(10));
+        recyclerAdapter = new RecyclerAdapter(6, R.layout.basket_row);
+        recyclerView.setAdapter(recyclerAdapter);
     }
 }
