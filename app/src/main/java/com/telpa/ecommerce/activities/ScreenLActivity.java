a0baf846a4816faf4d1ac1b7e120504b23566f25
volkan <volkansahin95@gmail.com>
2016-07-21 16:44:04 +0300

    screen l activity olusturuldu

---
+++ app/src/main/java/com/telpa/ecommerce/activities/ScreenLActivity.java
@@ -0,0 +1,15 @@
+package com.telpa.ecommerce.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.telpa.ecommerce.R;
+
+public class ScreenLActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_l);
+    }
+}
