fbe260a6484259ccd53caa91d3591ed0613d65b4
mrtblt <bulutmert95@hotmail.com>
2016-07-22 11:19:32 +0300

    Activity L geliştirilmesi

--- app/src/main/java/com/telpa/ecommerce/activities/ScreenLActivity.java
+++ app/src/main/java/com/telpa/ecommerce/activities/ScreenLActivity.java
@@ -4,12 +4,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.telpa.ecommerce.R;
+import com.telpa.ecommerce.utils.BaseActivity;
 
-public class ScreenLActivity extends AppCompatActivity {
+public class ScreenLActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen_l);
+
+
+        fcreateTitle("Product" );
+        fcreateToolbar(this,false,false,false,R.id.toolbar);
+        fcreateMenu(this, true);
+
+
+
+
     }
 }
