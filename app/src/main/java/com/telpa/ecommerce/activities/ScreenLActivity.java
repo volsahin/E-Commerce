c62912673fdbaad44e1e12ba6f52d166841c8602
mrtblt <bulutmert95@hotmail.com>
2016-07-22 15:23:34 +0300

    Activity L geliştirilmesi

--- app/src/main/java/com/telpa/ecommerce/activities/ScreenLActivity.java
+++ app/src/main/java/com/telpa/ecommerce/activities/ScreenLActivity.java
@@ -1,12 +1,21 @@
 package com.telpa.ecommerce.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.View;
 
 import com.telpa.ecommerce.R;
+import com.telpa.ecommerce.adapters.RecyclerAdapter;
 import com.telpa.ecommerce.utils.BaseActivity;
 
+
+
 public class ScreenLActivity extends BaseActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter recyclerAdapter;
+    private RecyclerView.LayoutManager recyclerLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,11 +23,28 @@ public class ScreenLActivity extends BaseActivity {
         setContentView(R.layout.activity_screen_l);
 
 
-        fcreateTitle("Product" );
-        fcreateToolbar(this,false,false,false,R.id.toolbar);
+        fcreateTitle("Product");
+        fcreateToolbar(this,false,true,false,R.id.toolbar);
         fcreateMenu(this, true);
 
+        View rootView = getLayoutInflater().inflate(R.layout.activity_screen_l,null);
 
+
+        recyclerView = (RecyclerView) rootView.findViewById(R.id.recycler_view_l);
+        recyclerView.setHasFixedSize(true);
+
+/*
+        recyclerLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
+        recyclerView.setLayoutManager(recyclerLayoutManager);
+
+*/
+
+        recyclerLayoutManager = new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(recyclerLayoutManager);
+
+        recyclerView.addItemDecoration(new RecyclerAdapter.SpaceItemDecoration(10));
+        recyclerAdapter = new RecyclerAdapter(10, R.layout.item_l_comment_row);
+        recyclerView.setAdapter(recyclerAdapter);
 
 
     }
