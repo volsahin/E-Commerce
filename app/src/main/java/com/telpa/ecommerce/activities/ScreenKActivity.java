62f3c770480ed65c2781e57676aba8f2a9081be2
volkan <volkansahin95@gmail.com>
2016-07-25 08:53:17 +0300

    screen f picture centered

--- app/src/main/java/com/telpa/ecommerce/activities/ScreenKActivity.java
+++ app/src/main/java/com/telpa/ecommerce/activities/ScreenKActivity.java
@@ -3,11 +3,14 @@ package com.telpa.ecommerce.activities;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.telpa.ecommerce.R;
 import com.telpa.ecommerce.adapters.ViewPagerAdapter;
 import com.telpa.ecommerce.adapters.ViewPagerAdapterK;
 import com.telpa.ecommerce.utils.BaseActivity;
+import com.viewpagerindicator.CirclePageIndicator;
+import com.viewpagerindicator.IconPageIndicator;
 import com.viewpagerindicator.TitlePageIndicator;
 
 public class ScreenKActivity extends BaseActivity {
@@ -24,9 +27,13 @@ public class ScreenKActivity extends BaseActivity {
 
         pager = (ViewPager)findViewById(R.id.viewPagerK);
         pager.setAdapter(new ViewPagerAdapterK(getSupportFragmentManager(),4));
-
+/*
         TitlePageIndicator titlePageIndicator = (TitlePageIndicator)findViewById(R.id.viewPagerIndicatorK);
         titlePageIndicator.setViewPager(pager);
+  */
+        CirclePageIndicator mIndicator = (CirclePageIndicator)findViewById(R.id.indicator);
+        mIndicator.setViewPager(pager);
+
 
     }
 }
