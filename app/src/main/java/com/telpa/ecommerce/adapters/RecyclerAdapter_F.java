626d42d241e439a0a3e21e57eb8606ddc7a43b72
gzmilgar <gzmilgar@gmail.com>
2016-08-11 09:36:13 +0300

    ScreenF small option_menu

--- app/src/main/java/com/telpa/ecommerce/adapters/RecyclerAdapter_F.java
+++ app/src/main/java/com/telpa/ecommerce/adapters/RecyclerAdapter_F.java
@@ -1,19 +1,26 @@
 package com.telpa.ecommerce.adapters;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Rect;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.telpa.ecommerce.R;
+import com.telpa.ecommerce.activities.ScreenKActivity;
+import com.telpa.ecommerce.activities.ScreenMActivity_Basket;
 import com.telpa.ecommerce.models.Product;
 
 import java.util.ArrayList;
+
 
 
 /**
@@ -32,6 +39,7 @@ public class RecyclerAdapter_F extends RecyclerView.Adapter<RecyclerAdapter_F.Vi
         this.id = id;
         this.products=products;
         this.activity=activity;
+
     }
 
     @Override
@@ -41,6 +49,7 @@ public class RecyclerAdapter_F extends RecyclerView.Adapter<RecyclerAdapter_F.Vi
         RecyclerAdapter_F.ViewHolder viewHolder = new RecyclerAdapter_F.ViewHolder(v);
         new SpaceItemDecoration(300);
         return viewHolder;
+
     }
 
     @Override
@@ -54,6 +63,45 @@ public class RecyclerAdapter_F extends RecyclerView.Adapter<RecyclerAdapter_F.Vi
         // holder.basketButton.setImageResource();
         holder.likeButton.setImageResource(R.drawable.ic_favorite_red_24dp);
 
+
+        holder.actionButton.setImageResource(R.drawable.ic_more_vert_white_24dp);
+        holder.actionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+               PopupMenu popup = new PopupMenu(activity, v);
+
+                popup.getMenuInflater().inflate(R.menu.option_menu, popup.getMenu());
+
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.item_Favorite:
+                                Toast.makeText(activity, "Favorite", Toast.LENGTH_SHORT).show();
+                                return true;
+                            case R.id.item_AddBasket:
+                                Intent intent2=new Intent();
+                                intent2.setClass(activity, ScreenMActivity_Basket.class);
+                                activity.startActivity(intent2);
+                                return true;
+                            case R.id.item_Detail:
+                                Intent intent3=new Intent();
+                                intent3.setClass(activity,ScreenKActivity.class);
+                                activity.startActivity(intent3);
+                                return true;
+                        }
+                        return true;
+                    }
+
+                });
+
+                popup.show();
+
+            }
+       });
+
+
+
     }
 
     @Override
@@ -63,11 +111,14 @@ public class RecyclerAdapter_F extends RecyclerView.Adapter<RecyclerAdapter_F.Vi
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
+
         public TextView productName;
         public TextView price;
         public ImageView image;
         public ImageButton basketButton;
         public ImageButton likeButton;
+        public ImageButton actionButton;
+
 
         public ViewHolder(View v) {
             super(v);
@@ -76,7 +127,13 @@ public class RecyclerAdapter_F extends RecyclerView.Adapter<RecyclerAdapter_F.Vi
             image = (ImageView) v.findViewById(R.id.imageView);
             basketButton = (ImageButton) v.findViewById(R.id.basketButton);
             likeButton = (ImageButton) v.findViewById(R.id.likeButton);
+            actionButton = (ImageButton) v.findViewById(R.id.actionButton);
+
+
+
+
         }
+
     }
 
     public static class SpaceItemDecoration extends RecyclerView.ItemDecoration {
@@ -95,6 +152,7 @@ public class RecyclerAdapter_F extends RecyclerView.Adapter<RecyclerAdapter_F.Vi
 
 
         }
+
     }
 
 }
