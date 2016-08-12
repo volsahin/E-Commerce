9aa798f1e1a03546ffbeeffc07466ec4782d5cd7
gzmilgar <gzmilgar@gmail.com>
2016-08-12 14:30:17 +0300

    padding düzenleme

---
+++ app/src/main/java/com/telpa/ecommerce/activities/ScreenA/IScreenAPresenter.java
@@ -0,0 +1,12 @@
+package com.telpa.ecommerce.activities.ScreenA;
+
+import com.telpa.ecommerce.models.Category;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Gizem İlgar on 12.8.2016.
+ */
+public interface IScreenAPresenter {
+    void ScreenA(ArrayList<Category> categories);
+}
---
+++ app/src/main/java/com/telpa/ecommerce/activities/ScreenA/ScreenAPresenter.java
@@ -0,0 +1,15 @@
+package com.telpa.ecommerce.activities.ScreenA;
+
+import com.telpa.ecommerce.models.Category;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Gizem İlgar on 12.8.2016.
+ */
+public class ScreenAPresenter implements IScreenAPresenter{
+    @Override
+    public void ScreenA(ArrayList<Category> categories) {
+
+    }
+}
