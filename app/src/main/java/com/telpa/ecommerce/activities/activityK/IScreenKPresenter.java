e3e4c742b12590981fe52a0c056d474d66caa688
CBaldemir <comertbaldemir@gmail.com>
2016-08-17 10:20:43 +0300

    ScreenK mvp radiobuttonlar düzenlendi..

--- app/src/main/java/com/telpa/ecommerce/activities/activityK/IScreenKPresenter.java
+++ app/src/main/java/com/telpa/ecommerce/activities/activityK/IScreenKPresenter.java
@@ -1,5 +1,7 @@
 package com.telpa.ecommerce.activities.activityK;
 
+import android.widget.RadioGroup;
+
 import com.telpa.ecommerce.models.BasketItem;
 import com.telpa.ecommerce.models.Comment;
 
@@ -9,6 +11,7 @@ import java.util.ArrayList;
  * Created by root on 15.08.2016.
  */
 public interface IScreenKPresenter {
+    void RadioGroup(RadioGroup radioGroup1, RadioGroup radioGroup2);
     ArrayList<Comment> fillList();
     ArrayList<BasketItem> basketitem();
     int favoritem();
